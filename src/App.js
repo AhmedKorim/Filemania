61afaa901843ccadb8543130b7ef2245c8e221ad
ahmedkorim <ahmedkorrim@gmail.com>
2018-08-15 12:09:21 +0300

    trying css modules

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Paper from "@material-ui/core/Paper/Paper";
+import Typography from "@material-ui/core/Typography/Typography";
+
+
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <Paper classes={'paper-here'}>
+                <Typography>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad architecto consectetur temporibus vel. Accusamus aut cum dolorum eos est illum
+                    impedit ipsam maxime numquam quaerat quam quidem, sapiente tempore voluptates!
+                </Typography>
+            </Paper>
+        );
+    }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import {createMuiTheme} from "@material-ui/core";
+import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const theme = createMuiTheme({
+    palette: {
+        type: 'dark'
+    }
+})
+
+ReactDOM.render(
+    <MuiThemeProvider theme={theme}>
+        <App/>
+    </MuiThemeProvider>
+    , document.getElementById('root'));
 registerServiceWorker();
